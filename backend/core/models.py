512f4e9636f2d9505c2622931dc439c358a22383
Kevin <kevin.arriaga@gmail.com>
2021-04-25 12:47:40 -0400

    Added user models and the user manager.

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+    PermissionsMixin
 
-# Create your models here.
+
+class BusinessType(models.Model):
+    """Model that defines the type of business"""
+    nombre = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Creates and saves a new user"""
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Creates and saves a new super user"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Custom user model that supports using email instead of username"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    business_name = models.CharField("nombre de la empresa", max_length=255)
+    business_type = models.ManyToManyField('BusinessType')
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
